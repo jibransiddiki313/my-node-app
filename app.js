d39bb88aaad75ee20aeec484115152b5aa5fa21f
JIBRAN SIDDIKI <121167403+jibransiddiki313@users.noreply.github.com>
2026-06-15 14:49:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello! This is Jibran Irfan Siddiki - DevOps Engineer And Aws solution certified! v2.o')
+    res.send('Hello! This is Jibran Irfan Siddiki - DevOps Engineer!')
 })
 
 app.get('/health', (req, res) => {
@@ -10,5 +10,4 @@ app.get('/health', (req, res) => {
 })
 
 const PORT = process.env.PORT || 8080
-    app.listen(PORT)
-})
+app.listen(PORT)
